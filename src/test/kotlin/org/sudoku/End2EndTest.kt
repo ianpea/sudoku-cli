9cc6aa71f728e432b4e3a61c9aa7a47f9c4f62f1
ipwj <i.peewj@gmail.com>
2026-09-07 18:42:55 +0800

    Added more E2E test

--- src/test/kotlin/org/sudoku/End2EndTest.kt
+++ src/test/kotlin/org/sudoku/End2EndTest.kt
@@ -1,41 +1,76 @@
 package org.sudoku
 
-import org.sudoku.domain.board.PuzzleGenerator
+import org.sudoku.fixtures.SudokuFixtures
 import org.sudoku.integration.FakeGameIO
+import org.sudoku.integration.FakePuzzleGenerator
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class End2EndTest {
     @Test
-    fun `player can insert check and quit game`() {
+    fun `player can insert and win the game`() {
         val io = FakeGameIO(
             listOf(
-                "A3 4", "check", "quit"
+                "A1 5", "I9 9", "check", "quit"
             )
         )
 
+        val board = SudokuFixtures.solvableBoard()
         runGame(
-            io = io, puzzleGenerator = PuzzleGenerator()
+            io = io, puzzleGenerator = FakePuzzleGenerator(board)
+        )
+
+        assertTrue(io.output().contains("Inserted 5 to A1."))
+        assertTrue(io.output().contains("Cannot insert to pre-filled cell I9."))
+        assertTrue(
+            io.output().contains("You won!")
+        )
+    }
+
+    @Test
+    fun `player can insert, undo and quit the game`() {
+        val io = FakeGameIO(
+            listOf(
+                "A1 4", "undo", "check", "quit"
+            )
+        )
+
+        val board = SudokuFixtures.solvableBoard()
+        runGame(
+            io = io, puzzleGenerator = FakePuzzleGenerator(board)
         )
 
         // Below asserts are based on the fact that the board is dynamically generated,
         // and the insert command might generate one of the outcomes.
-        assertTrue(
-            io.output().contains("Inserted 4 to A3.")
-                    || io.output().contains("Cannot insert to pre-filled cell A3.")
-                    || io.output().contains("already exists in Row")
-                    || io.output().contains("already exists in Column")
-                    || io.output().contains("already exists in the same 3x3 subgrid")
-        )
+        assertTrue(io.output().contains("Inserted 4 to A1."))
+        assertTrue { io.output().contains("Undo successful") }
+        assertTrue { io.output().contains("No rule violations detected.") }
 
         assertTrue(
-            io.output().contains("No rule violations detected")
-                    || io.output().contains("already exists in Row")
-                    || io.output().contains("already exists in Column")
-                    || io.output().contains("already exists in the same 3x3 subgrid")
+            io.output().contains("Game session ended")
+        )
+    }
+
+    @Test
+    fun `player can view violations`() {
+        val io = FakeGameIO(
+            listOf(
+                "A1 4", "check", "quit"
+            )
         )
 
+        val board = SudokuFixtures.solvableBoard()
+        runGame(
+            io = io, puzzleGenerator = FakePuzzleGenerator(board)
+        )
 
-        assertTrue(io.output().contains("Game session ended"))
+        // Below asserts are based on the fact that the board is dynamically generated,
+        // and the insert command might generate one of the outcomes.
+        assertTrue(io.output().contains("Inserted 4 to A1."))
+        assertTrue { io.output().contains("already exists in") }
+
+        assertTrue(
+            io.output().contains("Game session ended")
+        )
     }
 }
